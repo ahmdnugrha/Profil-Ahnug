96aacaa307c016a0185d2bffbbc0f2924c5c7bec
ahmdnugrha <109496283+ahmdnugrha@users.noreply.github.com>
2024-09-17 17:17:04 +0700

    Add files via upload

---
+++ Profil.java
@@ -0,0 +1,13 @@
+package Nugraha;
+
+public class Profil {
+    public static void main(String[] args) {
+        String Nama = "Ahmad Nugraha";
+        String NIM = "12350114004";
+        String Hobi = "Olahraga";
+
+        System.out.println(Nama);
+        System.out.println(NIM);
+        System.out.println(Hobi);
+    }
+}
